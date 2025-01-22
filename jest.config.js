09e0c5befd53d23edfb1d3b015f06ec1809efe90
dzemil5 <60248650+dzemil5@users.noreply.github.com>
2025-01-23 00:54:03 +0100

    Dodato logovanje korisnika

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  setupFiles: ["./jest.setup.ts"],
+  preset: "ts-jest",
+  testEnvironment: "node",
+};
---
+++ src/types/user.d.ts
@@ -0,0 +1,6 @@
+export interface TokenPayload {
+    userId: number;
+    email: string;
+    name: string;
+  }
+  
