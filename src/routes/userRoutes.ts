3c92afe3ad8c9bb0d3ed51de82225b4a98505ba7
dzemil5 <60248650+dzemil5@users.noreply.github.com>
2025-01-23 10:01:49 +0100

    Komentarisanje koda

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,24 +1,28 @@
-import { Router } from 'express';
-import { registerUser, loginUser, getUserProfile } from '../controllers/userController';
-import { authMiddleware } from '../middlewares/authMiddleware';
-import { errorMiddleware } from '../middlewares/errorMiddleware';
-import { PrismaClient } from '@prisma/client';
+import { Router } from "express";
+import {
+  registerUser,
+  loginUser,
+  getUserProfile,
+} from "../controllers/userController";
+import { authMiddleware } from "../middlewares/authMiddleware";
+import { errorMiddleware } from "../middlewares/errorMiddleware";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const router: Router = Router();
 
 /**Registruj korisnika */
-router.post('/register', registerUser);
+router.post("/register", registerUser);
 
 /**Logovanje */
-router.post('/login', loginUser);
+router.post("/login", loginUser);
 
 /**Vrati profil korisnika (prima Bearer token) */
-router.get('/profile', authMiddleware, getUserProfile);
+router.get("/profile", authMiddleware, getUserProfile);
 
 /**Vrati sve korisnike */
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const users = await prisma.user.findMany({
       select: {
@@ -29,12 +33,12 @@ router.get('/', async (req, res) => {
     });
     res.json(users);
   } catch (error) {
-    res.status(500).json({ error: 'Unable to fetch users' });
+    res.status(500).json({ error: "Unable to fetch users" });
   }
 });
 
 /**Vrati korisnika po ID */
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   const userId = parseInt(req.params.id, 10);
 
   try {
@@ -44,20 +48,20 @@ router.get('/:id', async (req, res) => {
     });
 
     if (!user) {
-      res.status(404).json({ message: 'User not found' });
+      res.status(404).json({ message: "User not found" });
       return;
     }
 
     res.json(user);
   } catch (error) {
-    res.status(500).json({ error: 'Unable to fetch user' });
+    res.status(500).json({ error: "Unable to fetch user" });
   }
 });
 
-//**Azuriraj korisnicki profil */ 
-router.put('/profile', authMiddleware, async (req, res) => {
+//**Azuriraj korisnicki profil */
+router.put("/profile", authMiddleware, async (req, res) => {
   if (!req.user) {
-    res.status(401).json({ message: 'Unauthorized' });
+    res.status(401).json({ message: "Unauthorized" });
     return;
   }
 
@@ -68,16 +72,18 @@ router.put('/profile', authMiddleware, async (req, res) => {
       where: { id: req.user.id },
       data: { name, email },
     });
-    res.status(200).json({ message: 'Profile updated successfully', updatedUser });
+    res
+      .status(200)
+      .json({ message: "Profile updated successfully", updatedUser });
   } catch (error) {
-    res.status(500).json({ error: 'Unable to update user profile' });
+    res.status(500).json({ error: "Unable to update user profile" });
   }
 });
 
 /**Izbrisi korisnika */
-router.delete('/profile', authMiddleware, async (req, res) => {
+router.delete("/profile", authMiddleware, async (req, res) => {
   if (!req.user) {
-    res.status(401).json({ message: 'Unauthorized' });
+    res.status(401).json({ message: "Unauthorized" });
     return;
   }
 
@@ -85,9 +91,9 @@ router.delete('/profile', authMiddleware, async (req, res) => {
     await prisma.user.delete({
       where: { id: req.user.id },
     });
-    res.status(200).json({ message: 'User deleted successfully' });
+    res.status(200).json({ message: "User deleted successfully" });
   } catch (error) {
-    res.status(500).json({ error: 'Error deleting user' });
+    res.status(500).json({ error: "Error deleting user" });
   }
 });
 
